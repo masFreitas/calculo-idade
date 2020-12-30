f79d23238439a67d05c48fd587b2579c1ecea861
Mateus Arthur da Silva de Freitas <65245536+masFreitas@users.noreply.github.com>
2020-12-30 18:52:00 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ExercicioDois
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // FACA UM ALGORITMO QUE LEIA O ANO DE NASCIMENTO E INFORME A IDADE
+
+            int anonasc, anoatual, idade;
+            Console.WriteLine("Informe o ano de seu nascimento (Ex: 1998):");
+            anonasc = int.Parse(Console.ReadLine());
+
+            anoatual = DateTime.Now.Year;
+
+            idade = anoatual - anonasc;
+
+            if(idade <= 1)
+            {
+                Console.WriteLine("Idade inválida");
+            }
+
+            else if (idade >=18)
+            {
+                Console.WriteLine("O usuário tem {0} anos - É de maior", idade);
+            }
+            else
+            {
+                Console.WriteLine("O usuário tem {0} anos - Não é de maior", idade);
+            }
+
+        }
+    }
+}
